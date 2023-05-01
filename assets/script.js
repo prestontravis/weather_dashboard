46d2d51980959c85a99aa88408f8fddb8566192d
prestontravis <119766277+prestontravis@users.noreply.github.com>
2023-04-30 22:54:09 -0400

    got script.js to display forecast

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,25 @@
 // get the necessary HTML elements
 const searchForm = document.getElementById('search-form');
+
+
+searchForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    const city = searchInput.value.trim();
+    if (!city) {
+      alert('Please enter a city name.');
+      return;
+    }
+    try {
+      const weatherData = await getWeatherData(city);
+      displayWeatherData(weatherData);
+      const forecastData = await getForecastData(city);
+      displayForecastData(forecastData);
+    } catch (error) {
+      alert('Unable to fetch weather data. Please try again later.');
+      console.error(error);
+    }
+  });
+  
 const searchInput = document.getElementById('search-city');
 const cityName = document.getElementById('city-name');
 const currentDate = document.getElementById('current-date');
@@ -8,7 +28,9 @@ const temperature = document.getElementById('temperature');
 const humidity = document.getElementById('humidity');
 const windSpeed = document.getElementById('wind-speed');
 const forecastDetails = document.getElementById('forecast-details');
-const searchHistoryList = document.getElementById('search-history-list');
+const searchHistoryList = document.getElementById('history-list');
+
+
 
 // create a function to fetch weather data from the API
 async function getWeatherData(city) {
@@ -19,7 +41,7 @@ async function getWeatherData(city) {
   return data;
 }
 
-// create a function to fetch the 5-day forecast data from the API
+// create a function to fetch the forecast data from the API
 async function getForecastData(city) {
   const apiKey = 'ac6fd43729738fba58989f34a3414481';
   const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
@@ -41,42 +63,57 @@ function displayWeatherData(data) {
 
 // create a function to display the forecast data
 function displayForecastData(forecastData) {
-  // Select the "forecast-details" element
-  const forecastDetails = document.querySelector('#forecast-details');
-
-  // Clear the previous forecast data
-  forecastDetails.innerHTML = '';
-
-  // Loop through the forecast data and create HTML elements for each forecast item
-  forecastData.list.forEach(forecast => {
-    // Create a div element to hold the forecast item
-    const forecastItem = document.createElement('div');
-    forecastItem.classList.add('forecast-item');
-
-    // Create a paragraph element to display the date and time
-    const date = new Date(forecast.dt * 1000);
-    const time = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-    const dateElement = document.createElement('p');
-    dateElement.classList.add('forecast-date');
-    dateElement.textContent = `${date.toLocaleDateString()} ${time}`;
-    forecastItem.appendChild(dateElement);
-
-    // Create an image element to display the weather icon
-    const iconUrl = `http://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
-    const iconElement = document.createElement('img');
-    iconElement.classList.add('forecast-icon');
-    iconElement.setAttribute('src', iconUrl);
-    forecastItem.appendChild(iconElement);
-
-    // Create a paragraph element to display the temperature
-    const temp = Math.round(forecast.main.temp);
-    const tempElement = document.createElement('p');
-    tempElement.classList.add('forecast-temp');
-    tempElement.textContent = `${temp} °F`;
-    forecastItem.appendChild(tempElement);
+    // Select the "forecast-details" element
+    const forecastDetails = document.querySelector('#forecast-details');
+  
+    // Clear the previous forecast data
+    forecastDetails.innerHTML = '';
+  
+    // Loop through the forecast data and create HTML elements for each day
+    for (let i = 0; i < forecastData.list.length; i += 8) {
+      // Get the forecast data for the current day
+      const forecast = forecastData.list[i];
+  
+      // Create a div element to hold the forecast item
+      const forecastItem = document.createElement('div');
+      forecastItem.classList.add('forecast-item');
+  
+      // Create a paragraph element to display the date
+      const date = new Date(forecast.dt * 1000);
+      const dateElement = document.createElement('p');
+      dateElement.classList.add('forecast-date');
+      dateElement.textContent = date.toLocaleDateString();
+      forecastItem.appendChild(dateElement);
+  
+      // Create an image element to display the weather icon
+      const iconUrl = `http://openweathermap.org/img/w/${forecast.weather[0].icon}.png`;
+      const iconElement = document.createElement('img');
+      iconElement.classList.add('forecast-icon');
+      iconElement.setAttribute('src', iconUrl);
+      forecastItem.appendChild(iconElement);
+  
+      // Create a paragraph element to display the temperature
+      const temp = Math.round(forecast.main.temp * 1.8 + 32);
+      const tempElement = document.createElement('p');
+      tempElement.classList.add('forecast-temp');
+      tempElement.textContent = `${temp} °F`;
+      forecastItem.appendChild(tempElement);
+  
+      // Create a paragraph element to display the humidity
+      const humidity = forecast.main.humidity;
+      const humidityElement = document.createElement('p');
+      humidityElement.classList.add('forecast-humidity');
+      humidityElement.textContent = `Humidity: ${humidity}%`;
+      forecastItem.appendChild(humidityElement);
+  
+      // Create a paragraph element to display the wind speed
+      const windSpeed = Math.round(forecast.wind.speed);
+      const windSpeedElement = document.createElement('p');
+      windSpeedElement.classList.add('forecast-wind-speed');
+      windSpeedElement.textContent = `Wind Speed: ${windSpeed} m/s`;
+      forecastItem.appendChild(windSpeedElement);
   
       // Add the forecast item to the "forecast-details" section
       forecastDetails.appendChild(forecastItem);
-    });
+    }
   }
-  
